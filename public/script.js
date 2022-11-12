1c05665d9807919ced77468186b2db87dd19eb86
Gwhiite <guibrancodecampos@gmail.com>
2022-11-12 17:53:13 -0300

    Testing payments with Stripe and NodeJS

---
+++ public/script.js
@@ -0,0 +1,25 @@
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  fetch("/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: [
+        { id: 1, quantity: 3 },
+        { id: 2, quantity: 1 },
+      ],
+    }),
+  })
+    .then((r) => {
+      if (r.ok) return r.json();
+      return r.json().then((json) => Promise.reject(json));
+    })
+    .then(({ url }) => {
+      window.location = url;
+    })
+    .catch((e) => {
+      console.error(e.error);
+    });
+});
